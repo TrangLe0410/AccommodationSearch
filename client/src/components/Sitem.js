61e4ab4bcc07e40beefc2633dee24c1a4ce5f977
TrangLe <letrangle2002@gmail.com>
2024-03-09 22:34:31 +0700

    api create post

--- client/src/components/Sitem.js
+++ client/src/components/Sitem.js
@@ -1,18 +1,21 @@
-import React, { memo } from 'react'
-import moment from 'moment'
-import 'moment/locale/vi'
-import { Link, useNavigate } from 'react-router-dom'
-import { formatVietnameseToString } from '../ultils/Common/formatVietnameseToString'
+import React, { memo } from 'react';
+import moment from 'moment';
+import 'moment/locale/vi';
+import { Link } from 'react-router-dom';
+import { formatVietnameseToString } from '../ultils/Common/formatVietnameseToString';
+
+const defaultImage = 'https://www.shutterstock.com/image-vector/landscape-photo-image-picture-placeholder-600nw-272872412.jpg';
+
 const Sitem = ({ title, price, image, createdAt, id }) => {
 
     const formatTime = (createdAt) => {
-        return moment(createdAt).fromNow()
+        return moment(createdAt).fromNow();
     }
 
     return (
         <div className='w-full flex items-center gap-2 py-2 border-b border-gray-300'>
             <img
-                src={image[0]}
+                src={image && image[0] ? image[0] : defaultImage}
                 alt="anh"
                 className='w-[65px] h-[65px] object-cover flex-none rounded-md'
             />
@@ -27,7 +30,7 @@ const Sitem = ({ title, price, image, createdAt, id }) => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default memo(Sitem)
+export default memo(Sitem);
