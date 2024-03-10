9207a02a1ed937de8ed374d39c01f3c192d74206
TrangLe <letrangle2002@gmail.com>
2024-03-10 19:08:52 +0700

    fix

---
+++ client/src/containers/System/ManagePost.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ManagePost = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-3xl font-medium py-4 border-b border-gray-200'>Quản lý bài đăng</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ManagePost
